7cc76defad005c253b656fe03905a3e309115a5e
Shubh Kaushik <shubhkaushik1002@gmail.com>
2025-08-20 00:57:19 +0530

    Time: 1 ms (99.82%), Space: 45.3 MB (92.01%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[n+1];
+        int dup = -1, missing = -1;
+        for (int num : nums) {
+            if (seen[num]) {
+                dup = num;
+            }
+            seen[num] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                missing = i;
+                break;
+            }
+        }
+        return new int[]{dup, missing};
+    }
+}
